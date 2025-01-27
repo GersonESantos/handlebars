e094231a9890381c7084cd09011e7a7b992f012f
GersonESS <gebhsantos@gmail.com>
2025-01-27 02:28:15 -0300

    a

---
+++ V0/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+
+app.get('/', (req, res) => {
+    res.render('body');
+    });
+    
+
+app.listen(8080, () => {
+    console.log('Rodando app listening at http://localhost:8080');
+  });
