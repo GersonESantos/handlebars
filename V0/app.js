bd9478d80aa0cd27a0e5b43e301951e363a74a03
GersonESS <gebhsantos@gmail.com>
2025-01-27 23:19:44 -0300

    Update app.js

--- V0/app.js
+++ V0/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
+
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
