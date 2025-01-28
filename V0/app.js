78ea16fe3ad20734d3b60a2ee2321d9c783f45d7
GersonESS <gebhsantos@gmail.com>
2025-01-27 23:05:56 -0300

    a

--- V0/app.js
+++ V0/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
--- V1/app.js
+++ V1/app.js
@@ -3,7 +3,7 @@ const app = express();
 const handlebars = require('express-handlebars');
 // config
 
-app.engine('handlebars', handlebars(defaultLayout: 'main'));
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 const exphbs = require('express-handlebars');
